41a819a1db9084b62ee4ba26d3b8ac161b6c27b0
Timo2203 <timo.schiller2203@gmail.com>
2019-03-11 09:55:10 +0100

    Schleife für Steinreihe #1

--- Minecraft setblock.py
+++ Minecraft setblock.py
@@ -4,10 +4,7 @@ from mcpi import block
 mc=Minecraft.create()
 
 def bodenplatte():
-    mc.setBlock(10,31,15,1)
-    mc.setBlock(11,31,15,1)
-    mc.setBlock(12,31,15,1)
-    mc.setBlock(13,31,15,1)
-    mc.setBlock(14,31,15,1)
-    mc.setBlock(15,31,15,1)
+   for i in range(8):
+       mc.setBlock(10+i,15,10,1)
+       
 bodenplatte()
