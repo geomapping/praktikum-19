19dc78c1331d23706462ad56ab0e36731125dc5b
Julian2401 <Julianbleyl2401@gmail.com>
2019-03-11 09:42:32 +0100

    Befehle in eigene Funktion gekapselt

--- Minecraft setblock.py
+++ Minecraft setblock.py
@@ -3,9 +3,11 @@ from mcpi import block
 
 mc=Minecraft.create()
 
-mc.setBlock(10,31,15,1)
-mc.setBlock(11,31,15,1)
-mc.setBlock(12,31,15,1)
-mc.setBlock(13,31,15,1)
-mc.setBlock(14,31,15,1)
-mc.setBlock(15,31,15,1)
+def bodenplatte():
+    mc.setBlock(10,31,15,1)
+    mc.setBlock(11,31,15,1)
+    mc.setBlock(12,31,15,1)
+    mc.setBlock(13,31,15,1)
+    mc.setBlock(14,31,15,1)
+    mc.setBlock(15,31,15,1)
+bodenplatte()
