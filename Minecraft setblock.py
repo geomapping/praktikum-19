1a45e8f68fed8946c913545ff8c1a82bd1422f34
Timo2203 <timo.schiller2203@gmail.com>
2019-03-11 09:35:15 +0100

    Erste version für Aufgabe #1

---
+++ Minecraft setblock.py
@@ -0,0 +1,11 @@
+from mcpi.minecraft import Minecraft
+from mcpi import block
+
+mc=Minecraft.create()
+
+mc.setBlock(10,31,15,1)
+mc.setBlock(11,31,15,1)
+mc.setBlock(12,31,15,1)
+mc.setBlock(13,31,15,1)
+mc.setBlock(14,31,15,1)
+mc.setBlock(15,31,15,1)
