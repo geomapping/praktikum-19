7af1b7bbe59fc42b44e0e4bf3cadef20ad383fc4
Timo2203 <timo.schiller2203@gmail.com>
2019-03-11 10:24:36 +0100

    Wände mit Variable für größe und Ausrichtung #2

---
+++ Minecraft.py
@@ -0,0 +1,24 @@
+from mcpi.minecraft import Minecraft
+from mcpi import block
+
+mc=Minecraft.create()
+wood=5
+stone=1
+x,y,z=mc.player.getPos()
+
+
+def Wände(xs,ys,zs,steps,ausrichtung,block):
+    if ausrichtung == x:
+        for b in range(steps):
+            for c in range(steps):
+                mc.setBlock(xs+c,ys+b,zs,block)
+    else:
+        for h in range(steps):
+            for k in range(steps):
+                mc.setBlock(xs,ys+h,zs+k,block)
+                
+
+Wände(10,35,15,8,z,stone)
+
+
+    
