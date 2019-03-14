3d5096018aee189f50edbc488e84ed844532bd85
Timo2203 <timo.schiller2203@gmail.com>
2019-03-14 09:02:13 +0100

    Fertigstellung des Programms #6

--- Minecraft Haus.py
+++ Minecraft Haus.py
@@ -15,6 +15,10 @@ def Bodenplatte(xs,ys,zs, steps,block):
         for i in range(steps):  
             mc.setBlock(xs+i,ys,zs+j,block)
 
+def Glassdach(xs,ys,zs, stepsG,block):
+    for j in range(stepsG):
+        for i in range(stepsG):  
+            mc.setBlock(xs+i,ys,zs+j,block)
 
 def Wand(xs,ys,zs,steps,ausrichtung,block):
     if ausrichtung == "g":
@@ -26,37 +30,37 @@ def Wand(xs,ys,zs,steps,ausrichtung,block):
             for k in range(steps):
                 mc.setBlock(xs,ys+h,zs+k,block)
 
-def fenster(xs,ys,zs,steps,ausrichtung,glass):
+def fenster(xs,ys,zs,stepsF,ausrichtung,glass):
     if ausrichtung == "g":
-        for b in range(steps):
-            for c in range(steps):
+        for b in range(stepsF):
+            for c in range(stepsF):
                 mc.setBlock(xs+c,ys+b,zs,glass)
     else:
-        for h in range(steps):
-            for k in range(steps):
+        for h in range(stepsF):
+            for k in range(stepsF):
                 mc.setBlock(xs,ys+h,zs+k,glass)
 
-def Tür(xs,ys,zs,steps,ausrichtung,block):
+def Tür(xs,ys,zs,stepsT,ausrichtung,block):
     if ausrichtung == "g":
-        for b in range(steps):
-            for c in range(steps):
+        for b in range(stepsT):
+            for c in range(stepsT):
                 mc.setBlock(xs+c,ys+b,zs,block)
     else:
-        for h in range(steps):
-            for k in range(steps):
+        for h in range(stepsT):
+            for k in range(stepsT):
                 mc.setBlock(xs,ys+h,zs+k,block)
 
-def Bauehaus (a,b,c):
-    Bodenplatte(a,b,c,8,wood)
-    Wand(a,b,c,8,"g",wood)
-    fenster(a+2,b+2,c,4,"g",glass)
-    Wand(a,b,c+7,8,"g",wood)
-    fenster(a+2,b+2,c+7,4,"g",glass)
-    Wand(a,b,c,8,"r",wood)
-    Tür(a,b+1,c+3,2,"r",air)
-    Wand(a+7,b,c,8,"r",wood)
-    Bodenplatte(a+1,b+7,c+1,6,glass)
+def Bauehaus (a,b,c,steps,stepsG,stepsF,stepsT):
+    Bodenplatte(a,b,c,steps,wood)
+    Wand(a,b,c,steps,"g",wood)
+    fenster(a+2,b+2,c,stepsF,"g",glass)
+    Wand(a,b,c+7,steps,"g",wood)
+    fenster(a+2,b+2,c+7,stepsF,"g",glass)
+    Wand(a,b,c,steps,"r",wood)
+    Tür(a,b+1,c+3,stepsT,"r",air)
+    Wand(a+7,b,c,steps,"r",wood)
+    Glassdach(a+1,b+7,c+1,stepsG,glass)
 
 
 #Befehle
-Bauehaus(40,20,40)
+Bauehaus(40,10,40,8,6,4,2)
